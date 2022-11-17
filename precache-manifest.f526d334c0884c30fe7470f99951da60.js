9c1b6336001960d21163546605766a09760b4212
Your Name <fatmaxala95@gmail.com>
2022-11-17 23:03:19 +0200

    Updates

---
+++ precache-manifest.f526d334c0884c30fe7470f99951da60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df52f0abd9b31b6b028d68607a3f6cf8",
+    "url": "/E-commerce-React/index.html"
+  },
+  {
+    "revision": "9d829f2b4ee280f11de2",
+    "url": "/E-commerce-React/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "5cfd98e0fc0208b413d7",
+    "url": "/E-commerce-React/static/css/main.08f11417.chunk.css"
+  },
+  {
+    "revision": "9d829f2b4ee280f11de2",
+    "url": "/E-commerce-React/static/js/2.ccbf7fc8.chunk.js"
+  },
+  {
+    "revision": "5cfd98e0fc0208b413d7",
+    "url": "/E-commerce-React/static/js/main.5db55973.chunk.js"
+  },
+  {
+    "revision": "5ca7665d5047015d3ea9",
+    "url": "/E-commerce-React/static/js/runtime~main.6c3d55e6.js"
+  },
+  {
+    "revision": "9b06bd0e5a28685d89d8592fd1af8fe3",
+    "url": "/E-commerce-React/static/media/logo.9b06bd0e.svg"
+  }
+]);
